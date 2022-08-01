b5d5981a5c36c58b1148b3acaf3f2b42368f0fbe
Gabriel <grossiwm@gmail.com>
2022-08-01 00:40:14 -0300

    more http methods for CharacterController

--- src/main/java/com/gabrielrossilopes/fetcher/controller/CharacterController.java
+++ src/main/java/com/gabrielrossilopes/fetcher/controller/CharacterController.java
@@ -1,12 +1,12 @@
 package com.gabrielrossilopes.fetcher.controller;
 
+import com.gabrielrossilopes.fetcher.model.Character;
 import com.gabrielrossilopes.fetcher.service.CharacterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/character")
@@ -24,4 +24,20 @@ public class CharacterController {
         return ResponseEntity.ok(characterService.findAll(pageNumber, pageSize));
     }
 
+    @GetMapping("/{id}")
+    private ResponseEntity<?> findCharacter(@PathVariable Integer id) {
+        return ResponseEntity.ok(characterService.findById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    private ResponseEntity<?> removeCharacter(@PathVariable Integer id) {
+        characterService.deleteCharacter(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping
+    private ResponseEntity<?> createCharacter(@RequestBody Character character) {
+        characterService.save(character);
+        return ResponseEntity.created(URI.create("/character/" + character.getId())).build();
+    }
 }
--- src/main/java/com/gabrielrossilopes/fetcher/service/CharacterService.java
+++ src/main/java/com/gabrielrossilopes/fetcher/service/CharacterService.java
@@ -1,5 +1,6 @@
 package com.gabrielrossilopes.fetcher.service;
 
+import com.gabrielrossilopes.fetcher.exception.CharacterNotFoundException;
 import com.gabrielrossilopes.fetcher.model.Character;
 import com.gabrielrossilopes.fetcher.repository.CharacterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CharacterService {
@@ -16,8 +18,8 @@ public class CharacterService {
     @Autowired
     private CharacterRepository characterRepository;
 
-    public void save(Character character) {
-        characterRepository.save(character);
+    public Character save(Character character) {
+        return characterRepository.save(character);
     }
 
     public List<Character> findAll() {
@@ -28,4 +30,20 @@ public class CharacterService {
         Pageable pageable = PageRequest.of(pageNumber, pageSize);
         return characterRepository.findAll(pageable);
     }
+
+    public Character findById(Integer id) {
+        Optional<Character> characterOptional = characterRepository.findById(id);
+        if (characterOptional.isEmpty())
+            throw new CharacterNotFoundException("Character of id " + id + " doesn't exist");
+        return characterOptional.get();
+    }
+
+    public void deleteCharacter(Integer id) {
+        Optional<Character> characaterOp = characterRepository.findById(id);
+        if (characaterOp.isEmpty()) {
+            throw new CharacterNotFoundException("Character of id " + id + " doesn't exist");
+        }
+        characterRepository.delete(characaterOp.get());
+    }
+
 }
